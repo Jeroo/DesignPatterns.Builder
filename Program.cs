4e752f273dff3032537d8e7dea5bea727696ad96
Jeroo <SALva18??>
2020-01-27 13:12:56 -0400

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DesignPatterns.Builder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sandwich = new Sandwich("Pan frica", "", "Carne", "Mayonesa", "Queso cheddar");
+
+            Console.WriteLine($"{sandwich.Bread} {sandwich.Condiments} {sandwich.Protein} {sandwich.Veggies}");
+            Console.ReadKey();
+        }
+    }
+}
